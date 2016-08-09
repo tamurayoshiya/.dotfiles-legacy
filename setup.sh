7faffd5913ecdca4fc91a65879bc2cd5bec3bc04
TAMURA Yoshiya <a@qmu.jp>
2016-08-09 19:38:17 +0900

    setup.sh修正

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cd ~/.dotfiles/files/
 for file in `ls -a`
 do
     if [ ! -e $HOME/$file ]; then
+        echo put $file symlink to home directory
         ln -s $HOME/.dotfiles/files/$file $HOME/$file
     fi
 done
